6ef1337ccb73a7b4d2c34b6ec217f8ca2a62b27f
Joey Volpe <thevolpe@gmail.com>
2015-04-27 16:57:42 -0700

    fixed heroku error

--- db/schema.rb
+++ db/schema.rb
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 20150427075751) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.string   "password_digest"
+    t.string   "pic_url"
   end
 
 end
