1ecfced9ea0925c2fbcdf05f7bae9a8afe9b8c6e
Joey Volpe <thevolpe@gmail.com>
2015-04-27 16:16:07 -0700

    finished project

---
+++ db/migrate/20150427075751_remove_pic_url_from_users.rb
@@ -0,0 +1,4 @@
+class RemovePicUrlFromUsers < ActiveRecord::Migration
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150426073025) do
+ActiveRecord::Schema.define(version: 20150427075751) do
 
   create_table "drinks", force: :cascade do |t|
     t.string   "name"
